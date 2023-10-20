77f05515b2bd30dd42ab6fb637ae289e86e06e8f
Graydon <graydonneill@Gmail.com>
2023-10-20 00:32:08 -0400

    Simplified bot

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,4 @@ COPY . /app
 
 RUN pip install -r requirements.txt
 
-CMD ["python", "./src/bot.py"]
+CMD ["python", "bot.py"]
--- bot.py
+++ bot.py
@@ -1,48 +1,20 @@
-import discord
 import os
-import random
+from discord import Intents
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
-
-#intents
-intents = discord.Intents.default()
-intents.message_content = True
-
-client = discord.Client(intents=intents)
 token = os.getenv('TOKEN')
 
-@client.event
-async def on_ready():
-    print("Logged in as a bot {0.user}".format(client))
+intents = Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.command()
+async def prompt(ctx, *args):
+    arguments = ' '. join(args)
+    await ctx.send(f'{len(args)} arguments: {arguments}')
 
 
-@client.event
-async def on_message(message):
-    username = str(message.author).split("#")[0]
-    channel = str(message.channel.name)
-    user_message = str(message.content)
-
-    print(f'Message {user_message} by {username} on {channel}')
-
-    if message.author == client.user:
-        return
-
-    if channel == "random":
-        if user_message.lower() == "hello" or user_message.lower() == "hi":
-            await message.channel.send(f'Hello {username}')
-            return
-        elif user_message.lower() == "bye":
-            await message.channel.send(f'Bye {username}')
-        elif user_message.lower() == "tell me a joke":
-            jokes = [" Can someone please shed more\
-                     light on how my lamp got stolen?",
-                     "Why is she called llene? She\
-            stands on equal legs.",
-                     "What do you call a gazelle in a \
-            lions territory? Denzel."]
-            await message.channel.send(random.choice(jokes))
-
-
-
-client.run(token)
+bot.run(token)
